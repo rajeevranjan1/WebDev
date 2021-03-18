ca4afc423d28a1ef69ca5059a5b02c607444494d
rranajn1 <icfai.rajeev@gmail.com>
2021-03-19 03:40:56 +0530

    updated path

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app=express();
-//app.use(express.static(__dirname+ '/web-app'));
+const path=require('path');
+app.use(express.static(path.join(__dirname,'/web-app')));
 
 //Routing
 app.get('/',function(req,res){
-    res.send("Response from Server...<br>Hello...");
+    //res.render('index');
     
 });
 app.listen(3000,function(){
