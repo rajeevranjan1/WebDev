c521027396885b6ad13b14f0907a3e06f4acd02c
rranajn1 <icfai.rajeev@gmail.com>
2021-03-19 01:58:45 +0530

    added server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app=express();
+app.use(express.static(__dirname+ '/web-app'));
+
+//Routing
+app.get('/',function(req,res){
+    res.send("Response from Server...<br>Hello...");
+    
+});
 app.listen(3000,function(){
     console.log("Server is Running on Port 3000...");
 });
